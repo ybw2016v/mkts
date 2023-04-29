e2f01fff5ba14bb6f6af866a9edf09eb5837c604
dogcraft <neko@neko.red>
2023-04-29 16:52:06 +0800

    update

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log('Misskey Translate Script v1.5');
+console.log('Misskey Translate Script v2.0');
 ApiUrl = 'https://test1-api.dogcraft.top/ts/';
 
 var cat = localStorage.getItem('lang');
@@ -13,39 +13,70 @@ lang_dog = lang_dog.substr(0, 2);
 
 function dog_add_fy(eldog) {
     //添加翻译按钮、区域以及绑定点击事件
+    if (eldog.className=='article') {
+        dog_add_fy_old(eldog);
+        return;
+    }
     if (eldog.fanyi == 1) {
-        console.log('已经添加过了，重复添加。')
+        // console.log('已经添加过了，重复添加。')
     } else {
-        var cl = document.createElement('div');
-        cl.className = '.clear';
-        var cl2 = document.createElement('div');
-        cl2.className = '.clear';
-        var dogfy = document.createElement('span');
-        dogfy.className = 'fanyi';
-        dogfy.ct = 0;
-        var dogbt = document.createElement('button');
-        dogbt.innerText = '翻译';
-        dogbt.className = 'button _button';
-        dogbt.style.backgroundColor = "rgba(0,0,200,0.5)";
+        const dogbt=document.createElement("button");
+        const btclass=Array.from(eldog.getElementsByTagName("footer")).slice(-1)[0].childNodes[0].getAttribute("class")
+        dogbt.setAttribute("class",btclass);
+        const nicon = document.createElement("i");
+        nicon.setAttribute("class", "ti ti-language");
+        dogbt.appendChild(nicon);
+        Array.from(eldog.getElementsByTagName("footer")).slice(-1)[0].appendChild(dogbt);
         dogbt.addEventListener('click', dog_fy);//绑定翻译函数
-        eldog.appendChild(cl);
-        eldog.appendChild(dogfy);
-        eldog.appendChild(cl2);
-        eldog.appendChild(dogbt);
+        const ctp = eldog.querySelector('div[style="container-type: inline-size;"]')
+        const ctix = ctp.getElementsByTagName('div')[0];
+        // console.log(ctix);
+        ctix.setAttribute("class", "fanyi-cont");
+        const fyc = document.createElement("div");
+        fyc.setAttribute("class", "fanyi");
+        fyc.ct=0;
+        ctp.appendChild(fyc);
         eldog.fanyi = 1;
     }
-
 }
+
+function dog_add_fy_old(eldog) {
+    if (eldog.fanyi == 1) {
+        // console.log('已经添加过了，重复添加。')
+        return;
+    } 
+    const dogbt=document.createElement("button");
+    const btclass=Array.from(eldog.getElementsByTagName("footer")).slice(-1)[0].getElementsByClassName("button")[0].getAttribute("class")
+    dogbt.setAttribute("class",btclass);
+    const nicon = document.createElement("i");
+    nicon.setAttribute("class", "ti ti-language");
+    dogbt.appendChild(nicon);
+    Array.from(eldog.getElementsByTagName("footer")).slice(-1)[0].appendChild(dogbt);
+    dogbt.addEventListener('click', dog_fy);//绑定翻译函数
+    const ctdog=eldog.getElementsByClassName('content')[0];
+    const ydog = ctdog.getElementsByClassName('text')[0];
+    ydog.setAttribute("class", "text fanyi-cont");
+    const fyc = document.createElement("div");
+    fyc.setAttribute("class", "fanyi");
+    fyc.ct=0;
+    ctdog.appendChild(fyc);
+    eldog.fanyi = 1;
+}
+
+
 
 async function dog_fy() {
     //从后端获得翻译文本并写入到html中
-    pdog = this.parentElement;
-    ldog = pdog.getElementsByClassName('fanyi');
-    if (ldog.length > 0) {
-        dog_fy_el = ldog[0];
+    // console.log('翻译中');
+    const pdog = this.parentElement.parentElement;
+
+    const ldog = pdog.getElementsByClassName('fanyi');
+
+    if (ldog) {
+        const dog_fy_el = ldog[0];
         if (dog_fy_el.ct == 0) {
-            // console.log('还没有翻译');
-            hdog = pdog.getElementsByClassName('havbbuyv')[0].innerText;
+            console.log('还没有翻译');
+            const hdog = pdog.getElementsByClassName('fanyi-cont')[0].innerText;
             post_dog = { 'c': hdog, 't': lang_dog };
             dog_fy_el.innerText = '正在翻译中……';
             uiy = await fetch(ApiUrl, {
@@ -63,24 +94,29 @@ async function dog_fy() {
             }
             dog_fy_el.innerText = `\n${res_dog}`;
             dog_fy_el.ct = 1
-            this.innerText = '收起翻译';
+            this.childNodes[0].setAttribute("class", "ti ti-language-off");
+            this.style="color: red;";
 
         } else {
+            // console.log('已经翻译过了');
             if (dog_fy_el.ct == 2) {
-                console.log(dog_fy_el.style.display)
                 dog_fy_el.style.display = "";
                 dog_fy_el.ct = 1;
-                this.innerText = '收起翻译';
+                this.childNodes[0].setAttribute("class", "ti ti-language-off");
+                this.style="color: red;";
             } else if (dog_fy_el.ct == 1) {
                 dog_fy_el.style.display = "none";
                 dog_fy_el.ct = 2;
-                this.innerText = '展开翻译';
+                this.childNodes[0].setAttribute("class", "ti ti-language");
+                this.style="color: blue;";
             }
         }
     } else {
         console.log('有地方不对劲');
     }
 }
+
+
 
 var config = { attributes: false, childList: true, subtree: true };
 sj = []
@@ -90,15 +126,18 @@ var callback = function (mutationsList) {
         if (item.type == 'childList') {
             for (let iy_dog = 0; iy_dog < item.addedNodes.length; iy_dog++) {
                 const iadog = item.addedNodes[iy_dog];
-                if (iadog.getElementsByClassName == undefined) {
+                if (iadog.getElementsByTagName == undefined) {
+                    // console.log('不是元素');
                 } else {
-                    sld = iadog.getElementsByClassName('content');
+                    // console.log('是元素');
+                    sld = iadog.getElementsByTagName('article');
                     if (sld.length > 0) {
                         for (let ct_dog = 0; ct_dog < sld.length; ct_dog++) {
                             const sdldog = sld[ct_dog];
-                            tty = sdldog.getElementsByClassName('text');
-                            if (tty.length > 0) {
-                                dog_add_fy(tty[0]);
+                            if (sdldog.parentElement.tagName!="A") {
+                                dog_add_fy(sdldog);
+                            } else {
+                                // console.log('不是要找的元素');
                             }
                         }
                     }
@@ -110,39 +149,7 @@ var callback = function (mutationsList) {
 
 
 function getar() {
-    var dogui = localStorage.getItem('ui');
-    if (dogui == null) {
-        localStorage.setItem("ui", "default");
-        dogui = "default";
-    }
-    if (vdog >= "12.100.0") {
-        if (dogui == "classic") {
-            var ar = document.getElementsByClassName("columns")[0];
-        }
-        else {
-            var ar = document.getElementsByClassName("dkgtipfy")[0];
-        }
-    }
-    else if (vdog >= "12.98.0") {
-        var ar = document.getElementsByClassName("dkgtipfy")[0];
-    }
-    else if (vdog >= "12.76.0") {
-        if (dogui == 'chat') {
-            var ar = document.getElementsByClassName("main")[0];
-        } else if (dogui == 'pope') {
-            var ar = document.getElementsByClassName("content")[0];
-        }
-        else {
-            if (vdog >= "12.95.0") {
-                var ar = document.getElementsByClassName("notes")[0];
-            } else {
-                var ar = document.getElementsByClassName("main")[0];
-            }
-        }
-    }
-    else {
-        var ar = (dogui == 'chat') ? document.getElementsByClassName("main")[0] : document.getElementsByClassName("content")[0];
-    }
+    ar=document.getElementById('misskey_app');
     if (ar == null) {
         console.log("没找到，等一秒");
         setTimeout(getar, 1000);
@@ -151,7 +158,9 @@ function getar() {
     else {
         for (let si = 0; si < sl.length; si++) {
             const sl_dog = sl[si];
-            dog_add_fy(sl_dog.getElementsByClassName('main')[0].getElementsByClassName('text')[0]);
+            if (sl_dog.parentElement.tagName!="A") {
+                dog_add_fy(sl_dog);
+            }
         }
         console.log("找到了");
         var observer = new MutationObserver(callback);
@@ -163,7 +172,6 @@ function getar() {
 
 window.onload = function () {
     console.log('页面加载完毕');
-    sl = document.getElementsByClassName('article');
+    sl = document.getElementsByTagName('article');
     getar();
 }
-
